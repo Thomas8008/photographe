cd5956a83b14aa970f93a1cbb29b6e326035cdab
Thomas8008 <148946181+Thomas8008@users.noreply.github.com>
2025-08-14 13:20:52 +0200

    Add files via upload

--- js/aos.js
+++ js/aos.js
@@ -12,12 +12,3 @@ const lightbox = GLightbox({
   closeButton: true
 });
 
-// Changer la couleur de la navbar au scroll
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
